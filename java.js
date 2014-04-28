da8585d8a7f797cb566ae5c8adcfaa3adf35c08e
MaRii-B <mariacruzbeltran25@gmail.com>
2014-04-28 15:42:50 -0500

    Commit
    primer commit

---
+++ java.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+var costo=0; 
+var resultado=0;
+var opcion=0;
+function valorcosto ()
+function valorresultado();
+switeh(opcion)
+{
+	cost=(costo/100*)50
+	cost=(costo/100*)25
+	case'1';
+	case'2';
+	case'3';
+	result=0
+	break;
+	case'
